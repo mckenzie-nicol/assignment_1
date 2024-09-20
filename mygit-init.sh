b66440b66153b46e793d6038ff67c4c69c73be05
Mckenzie Nicol <mnicol11@my.bcit.ca>
2024-09-20 14:05:37 -0700

    debug

--- mygit-init.sh
+++ mygit-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -gt 1 ]; then
-        echo "Usage: $0 <directory>"
+        echo "Usage: "$0" <directory>"
         exit 1
 fi
 
@@ -9,6 +9,6 @@ if [$# -eq 0]; then
         git init
         exit 0
 else
-        git init $1
+        git init "$1"
         exit 0
 fi
